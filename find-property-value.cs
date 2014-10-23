9970ee261df400f0890fe332f4119e1fa6cb5b60
espaciomore <espaciomore@gmail.com>
2014-10-23 16:31:07 -0400

    update fix; return all match

--- find-property-value.cs
+++ find-property-value.cs
@@ -5,7 +5,7 @@ namespace my_code_kata
 {
   partial static class my_code_kata
   {
-    protected static string FindPropertyValue(Object input, string value, string output = "")
+    protected static string FindPropertyValue(Object input, string value, ref List<string> retList, string output = "")
     {
         if (String.IsNullOrEmpty(output))
         {
@@ -13,41 +13,38 @@ namespace my_code_kata
         }
         var myType = input.GetType();
         var props = myType.GetProperties();
-    
+
         foreach (var prop in props)
         {
             // Debug.WriteLine(output + "." +prop.Name);
             Object propValue = prop.GetValue(input, null);
             object r = null;
-    
+
             if (propValue is String || propValue is Int32 || propValue is Decimal)
             {
-                if (prop.ToString() == value)
+                if (propValue.ToString() == value)
                 {
-                    return output + "=" + value;
+                    var match = output + "=" + value;
+                    retList.Add(match);
+                    return match;
                 }
-            } 
+            }
             else if (propValue is Array)
             {
-                foreach (var item in (Array)propValue)
+                foreach (var item in (Array) propValue)
                 {
-                    r = FindPropertyValue(item, value, output + "." + prop.Name);
+                    r = FindPropertyValue(item, value, ref retList, output + "." + prop.Name);
                 }
             }
             else
             {
                 if (propValue != null)
                 {
-                    r = FindPropertyValue(propValue, value, output + "." + prop.Name);
+                    r = FindPropertyValue(propValue, value, ref retList, output + "." + prop.Name);
                 }
             }
-    
-            if (r != null && (string) r != "")
-            {
-                return (string) r;
-            }
         }
-    
+
         return "";
     }
   }
