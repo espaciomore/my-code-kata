812f693352f9c79cb8fd94f8a3bab62e5b280654
espaciomore <espaciomore@gmail.com>
2014-10-28 12:50:10 -0400

    Update find-property-value.cs

--- find-property-value.cs
+++ find-property-value.cs
@@ -12,7 +12,7 @@ namespace my_code_kata
 {
   partial static class my_code_kata
   {
-    protected static string FindPropertyValue(Object input, string value, ref List<string> retList, string output = "")
+    protected static bool FindPropertyValue(Object input, string value, ref List<string> retList, string output = "")
     {
         if (String.IsNullOrEmpty(output))
         {
@@ -25,7 +25,6 @@ namespace my_code_kata
         {
             // Debug.WriteLine(output + "." +prop.Name);
             Object propValue = prop.GetValue(input, null);
-            object r = null;
 
             if (propValue is String || propValue is Int32 || propValue is Decimal)
             {
@@ -33,26 +32,25 @@ namespace my_code_kata
                 {
                     var match = output + "=" + value;
                     retList.Add(match);
-                    return match;
                 }
             }
             else if (propValue is Array)
             {
                 foreach (var item in (Array) propValue)
                 {
-                    r = FindPropertyValue(item, value, ref retList, output + "." + prop.Name);
+                    FindPropertyValue(item, value, ref retList, output + "." + prop.Name);
                 }
             }
             else
             {
                 if (propValue != null)
                 {
-                    r = FindPropertyValue(propValue, value, ref retList, output + "." + prop.Name);
+                    FindPropertyValue(propValue, value, ref retList, output + "." + prop.Name);
                 }
             }
         }
 
-        return "";
+        return retList.Count > 0;
     }
   }
 }
