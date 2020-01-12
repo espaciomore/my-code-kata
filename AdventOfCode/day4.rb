e4779e7b95d4db75525f34dea39c8a50a6d39728
Manuel A. Cerda R <manuel.cerda@introhive.com>
2020-01-12 17:11:26 -0400

    submit day 4

--- AdventOfCode/day4.rb
+++ AdventOfCode/day4.rb
@@ -1,53 +1,102 @@
 module Day4
 
-	def is_valid_password_in_range(p, r)
-		return false unless p >= r.first
-		return false unless p <= r.first
-		return true
+	def rule_1(password)
+		password.is_a?(Numeric) && password < 1000000
 	end
 
-	def is_valid_range(r)
-		return false unless r.first.to_s.match /^\d{6}$/
-		return false unless r.last.to_s.match /^\d{6}$/
-		return true
-	end	
-
-	def is_valid_password(p)
-		return false unless p.to_s.match /^[1-9]{6}$/
-		return false unless p.to_s.match /(1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,})/
-		return false unless p.to_s.match /^1*2*3*4*5*6*7*8*9*$/
-		return true
+	def rule_2(password, min=password, max=min)
+		min <= password && password <= max
 	end
 
-	def find_valid_passwords_in_range(r)
-		return false unless is_valid_range r
+	def rule_3(password)
+		digit = password.to_s.rjust(6, "0").split("").map { |c| c.to_i }
+		adjacent_digits = false
+		digit.each_cons(2) do |a,b|
+			if a == b
+				adjacent_digits = true
+			end
+		end
+		adjacent_digits
+	end
+
+	def rule_4(password)
+		digit = password.to_s.rjust(6, "0").split("").map { |c| c.to_i }
+		increasing_digits = true
+		digit.each_cons(2) do |a,b|
+			if a > b
+				increasing_digits = false
+			end
+		end
+		increasing_digits
+	end
+
+	def rule_5(password)
+		digit = password.to_s.rjust(6, "0").split("").map { |c| c.to_i }
+		groups = []
+		count = {}
+		digit.each_cons(2) do |a,b|
+			if a == b
+				groups << a unless groups.include?(a)
+				count[a.to_s] = count.has_key?(a.to_s) ? count[a.to_s] + 1 : 1
+			end
+		end
+		groups.any? { |d| count[d.to_s] == 1 }
+	end
+
+	def valid?(password)
+		rule_1(password.to_i) && rule_3(password.to_i) && rule_4(password.to_i) && rule_5(password.to_i)
+	end
+
+	def find_valid_passwords_in(range)
 		passwords = []
-		r.step(1) do |p|
-			passwords << p if is_valid_password p
-		end	
+		range.step(1) do |password|
+			passwords << password if valid? password
+		end
 		passwords	
 	end	
 
-	def count_valid_passwords_in_range(r)
-		find_valid_passwords_in_range( r ).size
+	def count_valid_passwords_in(range)
+		find_valid_passwords_in( range ).size
 	end
 		
 	def test
 		results =[]
-	    results << { :got => is_valid_password( '111111' ),
-	                 :expected => true }
-	    results << { :got => is_valid_password( '223450' ),
+	    results << { :got => valid?( '111111' ),
 	                 :expected => false }
-	    results << { :got => is_valid_password( '123789' ),
+			results << { :got => valid?( '990099' ),
+								 :expected => false }
+			results << { :got => valid?( '122345' ),
+								 :expected => true }
+	    results << { :got => valid?( '223450' ),
 	                 :expected => false }
-	    results << { :got => is_valid_password( '012345' ),
+	    results << { :got => valid?( '123879' ),
 	                 :expected => false }
-	    results << { :got => is_valid_password( '234566' ),
+	    results << { :got => valid?( '012345' ),
+	                 :expected => false }
+	    results << { :got => valid?( '234566' ),
 	                 :expected => true }  
-	    results << { :got => is_valid_password( '224433' ),
-	                 :expected => false }            
-	    results << { :got => count_valid_passwords_in_range( 307237..769058 ),
-	                 :expected => 889 }             
+	    results << { :got => valid?( '224433' ),
+	                 :expected => false }
+			results << { :got => valid?( '111233' ),
+								 :expected => true }
+			results << { :got => valid?( '111122' ),
+								 :expected => true }
+			results << { :got => valid?( '112233' ),
+								 :expected => true }
+			results << { :got => valid?( '122222' ),
+								 :expected => false }
+			results << { :got => valid?( '688889' ),
+								 :expected => false }
+			results << { :got => valid?( '677789' ),
+								 :expected => false }
+			results << { :got => valid?( '123444' ),
+								 :expected => false }
+			results << { :got => valid?( '122344' ),
+								 :expected => true }
+			results << { :got => valid?( '699999' ),
+								 :expected => false }
+			results << { :got => count_valid_passwords_in( (307237..769058) ),
+	                 :expected => 589 } # 889 589
 
 	    passed = []; failed = []
 
@@ -63,6 +112,6 @@ module Day4
 	end		
 end
 
-include Day4
-Day4.test
+# include Day4
+# puts Day4.test
 # EOF
