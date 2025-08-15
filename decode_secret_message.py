b128c54b5b00fec2dff7b93997e8f512a80d694c
Manuel Cerda <espaciomore@gmail.com>
2025-08-15 18:57:32 -0400

    copilot improvements

--- decode_secret_message.py
+++ decode_secret_message.py
@@ -2,36 +2,34 @@
 # pip install cssselect
 
 from lxml.html import parse
-from lxml.html import fromstring
+# from lxml.html import fromstring
 
 def decode_secret_message(url):
     doc = parse(url).getroot() # fromstring(url)
     parsedData = []
-    nRow = 0
-    nCol = 0
+    maxRow = 0
+    maxCol = 0
 
     for tbody in doc.cssselect('tbody'):
         for tr in tbody.cssselect('tr:not(:first-child)'):
             coordX = int(tr.cssselect('td:nth-child(1)')[0].text_content())
-            char = tr.cssselect('td:nth-child(2)')[0].text_content()
+            char = tr.cssselect('td:nth-child(2)')[0].text_content().strip()
             coordY = int(tr.cssselect('td:nth-child(3)')[0].text_content())
 
-            if coordX > nRow:
-                nRow = coordX
-            if coordY > nCol:
-                nCol = coordY
+            maxRow = max(maxRow, coordX)
+            maxCol = max(maxCol, coordY)
 
-            parsedData.append([coordX, char.strip(), coordY])
+            parsedData.append([coordX, char, coordY])
     
     matrix = []
 
-    for x in range(0, nRow + 1):
+    for x in range(0, maxRow + 1):
         matrix.append([])
-        for y in range(0, nCol + 1):
+        for y in range(0, maxCol + 1):
             matrix[x].append('')
 
-    for data in parsedData:
-        matrix[data[0]][data[2]] = data[1]
+    for x, char, y in parsedData:
+        matrix[x][y] = char
 
     matrixT = [list(row) for row in zip(*matrix)]
     
