a35eb9cce0e19e94440374ae830a0db9aa5bce65
Manuel Cerda <espaciomore@gmail.com>
2025-09-28 12:30:43 -0400

    update palindrome fonctions

--- AI/Types/palindrome.js
+++ AI/Types/palindrome.js
@@ -20,7 +20,7 @@
 // Tests:       > node server.js test ascii.test.dump
 //              [ [ [true/false], [f( x )] = [expected value] ] ]
 //
-// Data Schema: [{"(string) fname":{"(string) record":[(double) arg...]}}]
+// Data Schema: [{"(string) fname":{"(string) arg":[(double) pv...]}}]
 //
 this.palindrome = ( journey ) =>
 {
